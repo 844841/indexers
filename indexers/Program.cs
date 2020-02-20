f75267f3e353ac0a2d14b38f3cf392904f3cfd3f
15341a1205 <41323544+15341a1205@users.noreply.github.com>
2020-02-20 15:11:47 +0530

    Initial commit

---
+++ indexers/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace indexers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        static void indexdemo()
+        {
+            stdetails[0] = new Student
+            {
+                id = 444,
+                stdname = "aarya",
+                course = "c#"
+
+            };
+
+
+            stdetails[1] = new Student
+            {
+                id = 415,
+                stdname = "shakespeare",
+                course = "dotnet"
+
+            };
+
+            Console.WriteLine("{0}\t{1}\t{2}", stdetails[0].id, stdetails[0].stdname, stdetails[0].course);
+            Console.WriteLine("{0}\t{1}\t{2}", stdetails[1].id, stdetails[1].stdname, stdetails[1].course);
+            
+
+        }
+    }
+}
